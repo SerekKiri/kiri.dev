b6d954bf37659e7dbbde27c580e63045673b9303
SerekKiri <kacper.potyrala@gmail.com>
2021-03-18 10:16:37 +0100

    style: removed hidden text

--- src/components/description/index.tsx
+++ src/components/description/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 // Styles
-import { Wrapper, Hidden } from './styles'
+import { Wrapper } from './styles'
 
 const Description = () => {
     return (
         <Wrapper>
             20 y/o js/ts dev, space fanatic, ev and ai enthusiast 🐍
-            <Hidden>is rust too edgy? Golang to the moon!</Hidden>
         </Wrapper>
     )
 }
